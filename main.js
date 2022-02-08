2d83e8e2238a5e7c54b537ee36c90a1e1af913fa
Diego Brossard <brossarddiego@gmail.com>
2022-02-08 20:54:33 -0300

    Se incorpora arrays

--- main.js
+++ main.js
@@ -1,63 +1,65 @@
-var maquina = random(0, 2);
 var jugador;
-// Randomizacion del numero maquina
-function random(minimo, maximo) {
-  eleccionMaquina = Math.floor(Math.random() * (maximo - minimo + 1) + minimo);
-  return eleccionMaquina;
-}
-// Eleccion del jugador
+
+// Opciones del juego en Array
+const opciones = ["Piedra", "Papel", "Tijera"];
+// Se genera un numero random para la elección maquina
+const azar = Math.round(Math.random() * 2);
+
+// Eleccion del usuario (jugador)
 function eleccionJugador1() {
-  eleccion = parseInt(prompt("Piedra(1) - Papel(2) - Tijera(3)"));
-  if ((eleccion = 1)) {
-    jugador = 1;
+  eleccion = Number(prompt("Elije una opción: Piedra(1) - Papel(2) - Tijera(3)"));
+  if (eleccion == 1) {
     console.log("Has elegido PIEDRA");
-  } else if ((eleccion = 2)) {
-    jugador = 2;
+    jugador = 1;
+  } else if (eleccion == 2) {
     console.log("Has elegido PAPEL");
-  } else if ((eleccion = 3)) {
-    jugador = 3;
+    jugador = 2;
+  } else if (eleccion == 3) {
     console.log("Has elegido TIJERA");
+    jugador = 3;
   } else {
-    console.log("Elije una opción");
+    console.log("Elije una opción: Piedra(1) - Papel(2) - Tijera(3)");
+    eleccionJugador1();
   }
 }
-// Comparacion de valores
+
+// Se compara el valor del usuario y el generado por el random de la maquina
 function juego() {
-  random(0,2);
   eleccionJugador1();
   // piedra
-  if (jugador === 1 && maquina === 0) {
+  if (jugador === 1 && azar === 0) {
     console.log("La maquina elije PIEDRA");
     console.log("¡Hay empate!");
-  } else if (jugador === 1 && maquina === 1) {
+  } else if (jugador === 1 && azar === 1) {
     console.log("La maquina elije PAPEL");
     console.log("¡Gana la maquina!");
-  } else if (jugador === 1 && maquina === 2) {
+  } else if (jugador === 1 && azar === 2) {
     console.log("La maquina elije TIJERA");
     console.log("¡Ganaste!");
     // papel
-  } else if (jugador === 2 && maquina === 0) {
+  } else if (jugador === 2 && azar === 0) {
     console.log("La maquina elije PIEDRA");
     console.log("¡Ganaste!");
-  } else if (jugador === 2 && maquina === 1) {
+  } else if (jugador === 2 && azar === 1) {
     console.log("La maquina elije PAPEL");
     console.log("¡Hay empate!");
-  } else if (jugador === 2 && maquina === 2) {
+  } else if (jugador === 2 && azar === 2) {
     console.log("La maquina elije TIJERA");
     console.log("¡Gana la maquina!");
     // tijera
-  } else if (jugador === 3 && maquina === 0) {
+  } else if (jugador === 3 && azar === 0) {
     console.log("La maquina elije PIEDRA");
     console.log("¡Gana la maquina!");
-  } else if (jugador === 3 && maquina === 1) {
+  } else if (jugador === 3 && azar === 1) {
     console.log("La maquina elije PAPEL");
     console.log("¡Ganaste!");
-  } else if (jugador === 3 && maquina === 2) {
+  } else if (jugador === 3 && azar === 2) {
     console.log("La maquina elije TIJERA");
     console.log("¡Hay empate!");
   } else {
     console.log("Coloca un número");
+    eleccionJugador1();
+  }
 }
-}
-  
+
 juego();
